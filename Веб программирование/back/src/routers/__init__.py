1b56e4df9e36b6f7bd85cf1f40a1447a17c1dc86
gitububkm <ububkmart@gmail.com>
2025-11-28 23:22:20 +0300

    web fixing

--- Веб программирование/back/src/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.routers.users import router as users_router
-from src.routers.news import router as news_router
-from src.routers.comments import router as comments_router
-from src.routers.auth import router as auth_router
-
-__all__ = ["users_router", "news_router", "comments_router", "auth_router"]
-
